02e139fdf56186fe9a4daf38abd035478f85fd7b
hyoeun93 <hyoeun93@gmail.com>
2021-01-23 17:16:52 -0500

    increase broser efficiency

--- main.js
+++ main.js
@@ -3,19 +3,25 @@ const vertical = document.querySelector('.vertical');
 const horizontal = document.querySelector('.horizontal');
 const target = document.querySelector('.target');
 
-function handleMousemove(event) {
-  const x = event.x;
-  const y = event.y;
+addEventListener('load', () => {
+  const targetRect = target.getBoundingClientRect();
+  const targetHalfWidth = targetRect.width / 2;
+  const targetHalfHeight = targetRect.height / 2;
 
-  vertical.style.left = x + 'px';
-  horizontal.style.top = y + 'px';
+  function handleMousemove(event) {
+    const x = event.x;
+    const y = event.y;
 
-  target.style.left = x + 'px';
-  target.style.top = y + 'px';
+    vertical.style.transform = `translateX(${x}px)`;
+    horizontal.style.transform = `translateY(${y}px)`;
 
-  tag.style.left = x + 'px';
-  tag.style.top = y + 'px';
+    target.style.transform = `translate(${x - targetHalfWidth}px, ${
+      y - targetHalfHeight
+    }px)`;
 
-  tag.innerHTML = `${x}px, ${y}px`;
-}
-document.addEventListener('mousemove', handleMousemove);
+    tag.style.transform = `translate(${x}px, ${y}px)`;
+
+    tag.innerHTML = `${x}px, ${y}px`;
+  }
+  document.addEventListener('mousemove', handleMousemove);
+});
